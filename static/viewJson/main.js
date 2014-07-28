c80c0a4ad20849de1f4a91e17b74ab012612ee00
vagrant <vagrant@precise64.(none)>
2014-07-28 15:10:26 +0000

    Updated JSON Viewer with Template

--- static/viewJson/main.js
+++ static/viewJson/main.js
@@ -1,19 +1,7 @@
-var schema;
 var loadedJson;
 var maxIndex = 0;
 var content = document.getElementById('content');
 var error = document.getElementById("error");
-
-//Load schema
-$.ajax({
-  type:'GET',
-  dataType:'json',
-  async:false,
-  url:'schema.json',
-  success:function(data){
-    schema = data;
-  }
-});
 
 loadJson = function(url){
   $.ajax({
@@ -22,9 +10,12 @@ loadJson = function(url){
     url:url,
     success:function(data){
       loadedJson = data;
+    },
+    error:function(err){
+      error.innerHTML = "Could not load json : " + url;
     }
   });
-  if(loadedJson["@graph"]) maxIndex = loadedJson["@graph"].length - 1;
+  if(loadedJson) showElement(loadedJson);
 }
 
 showElement = function(obj){
@@ -35,35 +26,38 @@ showElement = function(obj){
 createTable = function(data){
   var table = document.createElement('table');
   var header = table.createTHead();
-  var headRow = header.insertRow();
 
   var headProp = document.createElement('th');
-  var headType = document.createElement('th');
+//  var headType = document.createElement('th');
   var headVal = document.createElement('th');
+
   headProp.innerHTML = 'Property';
-  headType.innerHTML = 'Expected Type';
+//  headType.innerHTML = 'Expected Type';
   headVal.innerHTML = 'Value';
 
   header.appendChild(headProp);
-  header.appendChild(headType);
+//  header.appendChild(headType);
   header.appendChild(headVal);
 
   for(var i in data){
-    //if(i.substr(0,1) == '@') continue;
 
     var row = table.insertRow();
     var cellProp = row.insertCell(-1);
-    var cellType = row.insertCell(-1);
+//    var cellType = row.insertCell(-1);
     var cellVal = row.insertCell(-1);
 
-    cellProp.innerHTML = '<a target="_blank" href="' + loadedJson['@context'] + '/' + i + '">' + i + '</a>';
-
-    if(schema.properties[i]){
-      var type = schema.properties[i].ranges[0];
-      cellType.innerHTML = '<a target="_blank" href="' + loadedJson['@context'] + '/' + type + '">' + type + '</a>';
+    if(data['@context']){
+        cellProp.innerHTML = '<a target="_blank" href="' + data['@context'] + '/' + i + '">' + i + '</a>';
     } else {
-      cellType.innerHTML = "unknown";
+        cellProp.innerHTML = i;
     }
+
+//    if(schema.properties[i]){
+//      var type = schema.properties[i].ranges[0];
+//      cellType.innerHTML = '<a target="_blank" href="' + loadedJson['@context'] + '/' + type + '">' + type + '</a>';
+//    } else {
+//      cellType.innerHTML = "unknown";
+//    }
 
     var valueType = Object.prototype.toString.call(data[i]);
     if(valueType === '[object Array]'){
@@ -85,27 +79,3 @@ document.getElementById("loadJsonButton").addEventListener("click", function(eve
   var url = document.getElementById("jsonLocation").value;
   loadJson(url);
 });
-
-document.getElementById("showElementButton").addEventListener("click", function(event){
-  error.innerHTML = "";
-  if(!loadedJson){
-    error.innerHTML = "No Json Loaded";
-  }
-  else {
-    if(loadedJson["@graph"]){
-      var index = document.getElementById("elementIndex").value;
-      index = parseInt(index);
-      if(index < 0 || index > maxIndex){
-        error.innerHTML = "Please only enter an integer in range 0 - " + maxIndex + " inclusively.";
-      } else {
-        showElement(loadedJson["@graph"][index]);
-      }
-    } else {
-      showElement(loadedJson);
-    }
-  }
-});
-
-function digitsOnly(obj) {
-  obj.value = obj.value.replace(/\D/g, "");
-}
