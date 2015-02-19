ddfd08cae6c2e9304c5d521476aa10fb2e695d8a
dmccarthy <dmccarthy@tssg.org>
2015-02-19 09:15:31 +0000

    Combined the 2 servers and rewrote start scripts

--- start_mongrel2.sh
+++ start_mongrel2.sh
@@ -1,5 +1,5 @@
 mkdir logs run tmp
 m2sh load -config cloudlet_platform.conf -db cloudlet_platform.db
-m2sh load -config http_cloudlet_platform.conf -db http_cloudlet_platform.db
-sudo m2sh start -db cloudlet_platform.db -every
-sudo m2sh start -db http_cloudlet_platform.db -every
+
+sudo m2sh start -db cloudlet_platform.db -name Http_Redirect
+sudo m2sh start -db cloudlet_platform.db -name Cloudlet_Platform
--- stop_mongrel2.sh
+++ stop_mongrel2.sh
@@ -1,2 +1,2 @@
-sudo m2sh stop -db cloudlet_platform.db -every
-sudo m2sh stop -db http_cloudlet_platform.db -every
+sudo m2sh stop -db cloudlet_platform.db -name Http_Redirect
+sudo m2sh stop -db cloudlet_platform.db -name Cloudlet_Platform
