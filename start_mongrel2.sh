7739e696a4601dd35b24baf6ae3c0a4195f87d53
dmccarthy <dmccarthy@tssg.org>
2015-05-21 16:34:36 +0100

    Ported latest OPENi fixes

--- start_mongrel2.sh
+++ start_mongrel2.sh
@@ -3,3 +3,4 @@ m2sh load -config cloudlet_platform.conf -db cloudlet_platform.db
 
 sudo m2sh start -db cloudlet_platform.db -name Http_Redirect
 sudo m2sh start -db cloudlet_platform.db -name Cloudlet_Platform
+sudo m2sh start -db cloudlet_platform.db -name Internal_Server
--- stop_mongrel2.sh
+++ stop_mongrel2.sh
@@ -1,2 +1,3 @@
 sudo m2sh stop -db cloudlet_platform.db -name Http_Redirect
 sudo m2sh stop -db cloudlet_platform.db -name Cloudlet_Platform
+sudo m2sh stop -db cloudlet_platform.db -name Internal_Server
